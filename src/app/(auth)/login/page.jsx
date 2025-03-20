295aabba3d64ce98f3884490eba767b25db453c0
Nathan Rose <contacttstyle@gmail.com>
2025-03-19 23:53:33 -0500

    Update page.jsx

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -3,14 +3,20 @@ import { handleGithubLogin } from "@/lib/action";
 import styles from "./login.module.css";
 
 const LoginPage = () => {
-
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <form action={handleGithubLogin}>
-          <button className={styles.github}>Login with Github</button>
-        </form>
-        <LoginForm />
+        <div className={styles.formCard}>
+          <form action={handleGithubLogin} className={styles.githubForm}>
+            <button className={styles.github}>Login with Github</button>
+          </form>
+          
+          <div className={styles.divider}>
+            <span>OR</span>
+          </div>
+          
+          <LoginForm />
+        </div>
       </div>
     </div>
   );
