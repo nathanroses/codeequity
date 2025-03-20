0b3a20167d0f1ea94e6bb52cfd68e8071024b35f
Nathan Rose <contacttstyle@gmail.com>
2025-03-19 23:08:34 -0500

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,28 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
-import Image from "next/image"; // Import Image component
-import Links from "./links/Links";
+import Image from "next/image";
 import styles from "./navbar.module.css";
 import { auth } from "@/lib/auth";
+import Links from "./links/Links";
 
 const Navbar = async () => {
   const session = await auth();
 
   return (
-    <div className={styles.container}>
-      <Link href="/" passHref>
-        <div className={styles.logo}>
-          <Image
-            src="/Code Equity Logo.png" // Path to your logo image in the public folder
-            alt="Logo" // Alt text for the logo
-            width={110} // Set the width as needed
-            height={110} // Set the height as needed
-          />
-        </div>
-      </Link>
-      <div>
+    <div className={styles.navbarWrapper}>
+      <div className={styles.container}>
+        <Link href="/" className={styles.logoLink}>
+          <div className={styles.logo}>
+            <Image
+              src="/Code Equity Logo.png"
+              alt="Code Equity Logo"
+              width={110}
+              height={110}
+              className={styles.logoImage}
+            />
+          </div>
+        </Link>
+        
         <Links session={session} />
       </div>
     </div>
